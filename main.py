01f1dfe1ca7f01c01ee402e028ea2991314e4e0a
FaisalAhmed <faisal.ahmed@nvision.es>
2024-12-01 15:53:13 +0100

    second commit

--- main.py
+++ main.py
@@ -7,6 +7,8 @@
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print("Hello World")
+    print('This is a test change')
 
 
 # Press the green button in the gutter to run the script.
